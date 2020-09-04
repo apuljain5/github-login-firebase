89024f9553f0f63d80f9769eac85b31a4c945f84
Apul Jain <apuljain5@gmail.com>
2020-09-04 21:29:55 +0530

    footer done and header logic is done

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from "./../../services/auth.service";
+import { ToastrService } from "ngx-toastr";
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-header',
@@ -6,10 +10,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  email = null;
 
-  constructor() { }
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+    private toastr: ToastrService
+  ) { 
+    auth.getUser().subscribe((user) => {
+      this.email = user?.email;
+    });
+  }
+
+
 
   ngOnInit(): void {
   }
 
+
+
+
+  async handleSignOut(){
+    try {
+      const res = await this.auth.signOut();
+      this.router.navigateByUrl('/signin');
+      this.toastr.info("Login Again to Continue");
+      this.email = null;
+    } catch (error) {
+      this.toastr.error('Something is wrong');
+    }
+  }
 }
